bc373d19b8a2893c96ffd3e8a3da7f89a8939ce2
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2023-10-18 16:51:47 +0700

    latihan 4

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php 
+
+class About {
+    public function index($nama = 'Shandhika', $pekerjaan = 'Dosen'){
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page(){
+        echo 'About/Page';
+    }
+}
+?>
